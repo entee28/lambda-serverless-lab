536fad0d14aed42157db62175e9dac1d4181622f
Thanh Nguyen <puffle.nnt@gmail.com>
2024-08-30 11:33:28 +0700

    containerized hello lambda

--- hello-service/index.js
+++ hello-service/index.js
@@ -1,24 +1,7 @@
-const serverless = require("serverless-http");
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/path", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-module.exports.handler = serverless(app);
+exports.handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("Hello, this is your Lambda function!"),
+  };
+  return response;
+};
